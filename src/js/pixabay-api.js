593aced4c293b042c2c80ecd1a3189007c43b2c2
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-04-15 19:17:10 +0200

    implementation of the project

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-export async function createRequest(dataSearch) {
+export async function createRequest(dataSearch, page) {
     const params = new URLSearchParams({
         key: "43244654-6a0ffb606fc9a226d05679f88",
         q: dataSearch,
         image_type: "photo",
         orientation: "horizontal",
-        safesearch: "true"
+        safesearch: "true",
+        page,
+        per_page: 15
     });
-    const response = await axios.get(`https://pixabay.com/api/?${params}`);
-    return response.data;
+    const {data} = await axios.get(`https://pixabay.com/api/?${params}`);
+    return data;
 }
--- src/main.js
+++ src/main.js
@@ -12,39 +12,79 @@ import { hideLoading } from "./js/render-functions";
 const form = document.querySelector(".search-form");
 const gallery = document.querySelector(".gallery");
 const loader = document.querySelector(".loading-indicator");
+const loadBtn = document.querySelector(".load-btn");
 
+let dataSearch = "";
+let page = 1;
+let allPages = 0;
 form.addEventListener("submit", handleSubmit);
+loadBtn.addEventListener("click", leadMore);
 
 async function handleSubmit(event) {
     event.preventDefault();
     gallery.innerHTML = "";
-    const dataSearch = event.currentTarget.elements.data.value.trim();
+    hideLoading(loadBtn);
+    dataSearch = event.currentTarget.elements.data.value.trim();
     if (dataSearch === "") {
-            return iziToast.error({
+        return iziToast.error({
             message: "Sorry, there are no images matching your search query. Please try again!",
-             position: "topRight"
-            });
+            position: "topRight"
+        });
     };
     showLoading(loader);
     try {
-        const data = await createRequest(dataSearch);
+        page = 1;
+        const data = await createRequest(dataSearch, page);
         if (data.hits.length === 0) {
-                hideLoading(loader);
-                return iziToast.error({
-                    message: "Sorry, there are no images matching your search query. Please try again!",
-                 position: "topRight"
-                })
-            }
-            form.reset();           
-            gallery.innerHTML = createGallery(data.hits);
-            lightboxGallery.refresh();
+            hideLoading(loader);
+            return iziToast.error({
+                message: "Sorry, there are no images matching your search query. Please try again!",
+                position: "topRight"
+            })
+        }
+        form.reset();
+        gallery.innerHTML = createGallery(data.hits);
+        allPages = Math.ceil(data.total / 15);
+        if (page < allPages) {
+            showLoading(loadBtn);
+        }
+        
+        lightboxGallery.refresh();
     } catch (error) {
-         iziToast.error({
+        iziToast.error({
+            message: "Sorry, there are no images matching your search query. Please try again!",
+            position: "topRight"
+        })
+    } finally {
+        hideLoading(loader);
+    }
+}
+
+async function leadMore(event) {
+    page += 1;
+    hideLoading(loadBtn);
+    showLoading(loader);
+    try {
+        const data = await createRequest(dataSearch, page);
+        gallery.insertAdjacentHTML("beforeend", createGallery(data.hits));
+        const { height } = gallery.firstElementChild.getBoundingClientRect();
+        window.scrollBy({
+            top: height * 2,
+            behavior: "smooth",
+        });
+
+        if (page < allPages) {
+            showLoading(loadBtn);
+        }
+        lightboxGallery.refresh();
+        
+    } catch (error) {
+        iziToast.error({
                 message: "Sorry, there are no images matching your search query. Please try again!",
                 position: "topRight"
                 })
     } finally {
-           hideLoading(loader);
+        hideLoading(loader);
     }
 }
 
